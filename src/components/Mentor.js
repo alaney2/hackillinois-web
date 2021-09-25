8d122ceb2679d461b12da0f69dcbdeb7f1b8a5f4
Alan Yao <alanyao.training@gmail.com>
2021-09-25 13:34:41 -0500

    Flex col mobile

--- src/components/Mentor.js
+++ src/components/Mentor.js
@@ -1,6 +1,6 @@
 function Mentor({ description, firstName, lastName, profile }) {
   return (
-    <div className="flex px-12 pt-12">
+    <div className="flex-col md:flex px-12 pt-12">
       <img src={profile} alt={`${firstName} ${lastName}`} className="w-36 rounded-full h-36 mr-6"/>
       <div className="flex-col">
         <h2 className="font-medium text-lg mb-2">{`${firstName} ${lastName}`}</h2>
